7e9c7730d7cf8329d8da7455386c10e05f14fe39
lights <lights@tutanota.de>
2018-10-11 15:16:37 -0400

    add migrations branch for core

---
+++ versions/9768bc970df0_init.py
@@ -0,0 +1,24 @@
+"""init
+
+Revision ID: 9768bc970df0
+Revises: 
+Create Date: 2018-10-11 15:14:18.646164
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '9768bc970df0'
+down_revision = None
+branch_labels = ('core',)
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
