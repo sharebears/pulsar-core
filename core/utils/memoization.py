175028ee79892336e2ae041eac485db994a4a2a3
lights <lights@tutanota.de>
2018-10-13 16:23:06 -0400

    docs

--- core/utils/memoization.py
+++ core/utils/memoization.py
@@ -3,6 +3,11 @@ from typing import Callable
 
 
 def cached_property(func: Callable) -> property:
+    """
+    A  decorator that caches the result for the duration of
+    the lifetime of the object in a private object property.
+    """
+
     @wraps(func)
     def wrapper(self):
         try:
