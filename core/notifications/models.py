ad5f40870d7ebbacc6a16325a741f474d052c1e2
lights <lights@tutanota.de>
2018-10-14 20:53:14 -0400

    add notificationtype model and change notifications to use a FK ID

--- core/notifications/models.py
+++ core/notifications/models.py
@@ -24,11 +24,16 @@ class Notification(db.Model, SinglePKMixin):
 
     id: int = db.Column(db.Integer, primary_key=True)
     user_id: int = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False, index=True)
-    type: str = db.Column(db.String, nullable=False, index=True)
+    type_id: int = db.Column(
+        db.Integer, db.ForeignKey('notifications_types.id'), nullable=False, index=True)
     time: datetime = db.Column(
         db.DateTime(timezone=True), nullable=False, server_default=func.now())
     contents: str = db.Column(JSONB, nullable=False)
     read: bool = db.Column(db.Boolean, nullable=False, server_default='f', index=True)
+
+    @property
+    def type(self):
+        return NotificationType.from_id(self.type_id).type
 
     @classmethod
     def new(cls,
@@ -92,3 +97,46 @@ class Notification(db.Model, SinglePKMixin):
             cls.__cache_key_notification_count__.format(user_id=user_id, type=t),
             cls.__cache_key_of_user__.format(user_id=user_id, type=t)
             ) for t in types])))
+
+
+class NotificationType(SinglePKMixin):
+    __tablename__ = 'notifications_types'
+    __cache_key__ = 'notifications_type_{id}'
+    __cache_key_id_of_type__ = 'notifications_type_{type}_id'
+    __cache_key_all__ = 'notifications_type_all'
+
+    id: int = db.Column(db.Integer, primary_key=True)
+    type: str = db.Column(db.String, nullable=False, unique=True, index=True)
+
+    @classmethod
+    def get_id_of_type(cls, type: str) -> 'NotificationType':
+        """
+        Get the ID of the notification type, and if the type is not in the database,
+        add it and return the new ID.
+
+        :param type: The notification type
+        :return:     The notification ID
+        """
+        return cls.from_query(
+            key=cls.__cache_key_id_of_type__.format(type=type),
+            filter=cls.type == type
+            ) or cls._new(type=type)
+
+    @classmethod
+    def get_type_of_id(cls, id: int) -> 'NotificationType':
+        """
+        Get a notification type from the database given an ID.
+
+        :param id: The notification ID
+        :return:   The corresponding notification type
+        """
+        return cls.from_pk(id)
+
+    @classmethod
+    def get_all_types(cls) -> List['NotificationType']:
+        """
+        Get all notification types in the database.
+
+        :return: All notification type objects
+        """
+        return cls.get_many(key=cls.__cache_key_all__, order=cls.id, limit=None)
