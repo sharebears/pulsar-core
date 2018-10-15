75f005ed43e2e46cde15c6dd141c25ca1ed48a83
lights <lights@tutanota.de>
2018-10-14 23:50:51 -0400

    remove /moderate from the user moderation route (do not need)

--- tests/users/test_view_moderate.py
+++ tests/users/test_view_moderate.py
@@ -10,7 +10,7 @@ from core.users.models import User
 
 def test_int_overflow(app, authed_client):
     add_permissions(app, 'moderate_users')
-    response = authed_client.put('/users/1/moderate', data=json.dumps({
+    response = authed_client.put('/users/1', data=json.dumps({
         'invites': 99999999999999999999999999,
         }))
     check_json_response(response, 'Invalid data: value must be at most 2147483648 (key "invites")')
@@ -18,7 +18,7 @@ def test_int_overflow(app, authed_client):
 
 def test_moderate_user(app, authed_client):
     add_permissions(app, 'moderate_users')
-    response = authed_client.put('/users/2/moderate', data=json.dumps({
+    response = authed_client.put('/users/2', data=json.dumps({
         'email': 'new@ema.il',
         'uploaded': 999,
         'downloaded': 998,
@@ -37,7 +37,7 @@ def test_moderate_user(app, authed_client):
 
 def test_moderate_user_incomplete(app, authed_client):
     add_permissions(app, 'moderate_users')
-    response = authed_client.put('/users/2/moderate', data=json.dumps({
+    response = authed_client.put('/users/2', data=json.dumps({
         'password': 'abcdefGHIfJK12#',
         }))
     check_json_response(response, {
@@ -52,7 +52,7 @@ def test_moderate_user_incomplete(app, authed_client):
 
 def test_moderate_user_not_found(app, authed_client):
     add_permissions(app, 'moderate_users')
-    response = authed_client.put('/users/10/moderate', data=json.dumps({
+    response = authed_client.put('/users/10', data=json.dumps({
         'email': 'new@ema.il',
         }))
     check_json_response(response, 'User 10 does not exist.')
@@ -67,7 +67,7 @@ def test_change_permissions(app, authed_client):
         """UPDATE user_classes
         SET permissions = '{"moderate_users", "moderate_users_advanced", "view_invites"}'""")
 
-    response = authed_client.put('/users/1/moderate', data=json.dumps({
+    response = authed_client.put('/users/1', data=json.dumps({
         'permissions': {
             'moderate_users': False,
             'change_password': False,
@@ -103,7 +103,7 @@ def test_change_permissions_failure(app, authed_client, permissions, expected):
     db.engine.execute(
         """UPDATE user_classes SET permissions = '{"legacy"}'
         WHERE name = 'User'""")
-    response = authed_client.put('/users/1/moderate', data=json.dumps({
+    response = authed_client.put('/users/1', data=json.dumps({
         'permissions': permissions}))
     check_json_response(response, expected)
 
@@ -111,7 +111,7 @@ def test_change_permissions_failure(app, authed_client, permissions, expected):
 def test_change_permissions_restricted(app, authed_client):
     """Basic but not advanced permissions privileges."""
     add_permissions(app, 'moderate_users')
-    response = authed_client.put('/users/1/moderate', data=json.dumps({
+    response = authed_client.put('/users/1', data=json.dumps({
         'permissions': {'moderate_users': False}}))
     check_json_response(
         response, 'Invalid data: moderate_users is not a valid permission (key "permissions")')
@@ -124,7 +124,7 @@ def test_change_forum_permissions(app, authed_client):
     db.engine.execute("""UPDATE user_classes
                       SET forum_permissions = '{"forums_forums_permission_2"}'""")
 
-    response = authed_client.put('/users/1/moderate', data=json.dumps({
+    response = authed_client.put('/users/1', data=json.dumps({
         'forum_permissions': {
             'forums_forums_permission_2': False,
             'forums_threads_permission_1': False,
@@ -149,7 +149,7 @@ def test_change_forum_permissions_failure(app, authed_client):
     db.engine.execute("""UPDATE user_classes
                       SET forum_permissions = '{"forums_forums_permission_2"}'""")
 
-    response = authed_client.put('/users/1/moderate', data=json.dumps({
+    response = authed_client.put('/users/1', data=json.dumps({
         'forum_permissions': {
             'forums_forums_permission_2': True,
             'forums_threads_permission_1': False,
@@ -166,7 +166,7 @@ def test_change_forum_permissions_failure(app, authed_client):
 
 @pytest.mark.parametrize(
     'endpoint, method', [
-        ('/users/1/moderate', 'PUT'),
+        ('/users/1', 'PUT'),
     ])
 def test_route_permissions(app, authed_client, endpoint, method):
     response = authed_client.open(endpoint, method=method)
